fd55600d16f85027c957ae5514bdac20553330ad
Michael Luo <michael.luo0801@gmail.com>
2019-01-24 18:46:49 +1100

    update container sty;e

--- myblog/src/components/Layout.js
+++ myblog/src/components/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Link } from 'gatsby';
+import styled from 'styled-components';
 
 import Header from '../core/Header';
 
 import { rhythm, scale } from '../core/utils/typography';
 
+const Container = styled.main`
+  padding: ${rhythm(4)} ${rhythm(6)};
+  background: white;
+`;
 class Layout extends React.Component {
   render() {
     const { location, title, children } = this.props;
@@ -13,11 +17,12 @@ class Layout extends React.Component {
     return (
       <div>
         <Header />
-        {children}
+        <Container>{children}</Container>
         <footer>
           ©
           {' '}
           {new Date().getFullYear()}
+          {' '}
 , Built with
           {' '}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
--- myblog/src/pages/blog.js
+++ myblog/src/pages/blog.js
@@ -19,7 +19,7 @@ class BlogIndex extends React.Component {
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug;
           return (
-            <div key={node.fields.slug}>
+            <article key={node.fields.slug}>
               <h3
                 style={{
                   marginBottom: rhythm(1 / 4),
@@ -31,7 +31,7 @@ class BlogIndex extends React.Component {
               </h3>
               <small>{node.frontmatter.date}</small>
               <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
+            </article>
           );
         })}
       </Layout>
