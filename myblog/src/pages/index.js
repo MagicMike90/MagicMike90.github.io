484090acab3691969b5dc5bebd743dbe6e4e9262
Michael Luo <michael.luo0801@gmail.com>
2019-01-24 11:37:57 +1100

    add parallax

--- myblog/src/pages/index.js
+++ myblog/src/pages/index.js
@@ -1,66 +1,112 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
+import ReactDOM from 'react-dom';
+import { Parallax, ParallaxLayer } from 'react-spring/addons';
 
-import Bio from '../components/Bio';
-import Layout from '../components/Layout';
-import SEO from '../components/seo';
-import { rhythm } from '../core/utils/typography';
+// Little helpers ...
+const url = (name, wrap = false) => `${wrap ? 'url(' : ''}https://awv3node-homepage.surge.sh/build/assets/${name}.svg${
+  wrap ? ')' : ''
+}`;
+const Pink = ({ children }) => <span style={{ color: '#FF6AC1' }}>{children}</span>;
+const Yellow = ({ children }) => <span style={{ color: '#EFF59B' }}>{children}</span>;
+const Lightblue = ({ children }) => <span style={{ color: '#9AEDFE' }}>{children}</span>;
+const Green = ({ children }) => <span style={{ color: '#57EE89' }}>{children}</span>;
+const Blue = ({ children }) => <span style={{ color: '#57C7FF' }}>{children}</span>;
+const Gray = ({ children }) => <span style={{ color: '#909090' }}>{children}</span>;
 
-class BlogIndex extends React.Component {
+export default class App extends React.Component {
   render() {
-    const { data } = this.props;
-    const siteTitle = data.site.siteMetadata.title;
-    const posts = data.allMarkdownRemark.edges;
-
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" keywords={['blog', 'gatsby', 'javascript', 'react']} />
-        <Bio />
-        {posts.map(({ node }) => {
-          const title = node.frontmatter.title || node.fields.slug;
-          return (
-            <div key={node.fields.slug}>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-              <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
-          );
-        })}
-      </Layout>
+      <Parallax ref={ref => (this.parallax = ref)} pages={3}>
+        <ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />
+        <ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />
+
+        <ParallaxLayer
+          offset={0}
+          speed={0}
+          factor={3}
+          style={{ backgroundImage: url('stars', true), backgroundSize: 'cover' }}
+        />
+
+        <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
+          <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
+          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
+          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
+          <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2.5}
+          speed={-0.4}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            pointerEvents: 'none',
+          }}
+        >
+          <img src={url('earth')} style={{ width: '60%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2}
+          speed={-0.3}
+          style={{
+            backgroundSize: '80%',
+            backgroundPosition: 'center',
+            backgroundImage: url('clients', true),
+          }}
+        />
+
+        <ParallaxLayer
+          offset={0}
+          speed={0.1}
+          onClick={() => this.parallax.scrollTo(1)}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+        >
+          <img src={url('server')} style={{ width: '20%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={1}
+          speed={0.1}
+          onClick={() => this.parallax.scrollTo(2)}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+        >
+          <img src={url('bash')} style={{ width: '40%' }} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={2}
+          speed={-0}
+          style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          onClick={() => this.parallax.scrollTo(0)}
+        >
+          <img src={url('clients-main')} style={{ width: '40%' }} />
+        </ParallaxLayer>
+      </Parallax>
     );
   }
 }
-
-export default BlogIndex;
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-          }
-        }
-      }
-    }
-  }
-`;
