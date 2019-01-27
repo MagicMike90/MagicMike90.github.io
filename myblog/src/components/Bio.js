87f8c52a18cdb3e735e52320eb8bd2f40cd011bd
Michael Luo <michael.luo0801@gmail.com>
2019-01-27 21:07:12 +1100

    add bio page

--- myblog/src/components/Bio.js
+++ myblog/src/components/Bio.js
@@ -2,6 +2,13 @@ import React from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import Image from 'gatsby-image';
 
+import styled from 'styled-components';
+import { Linkedin } from 'styled-icons/fa-brands/Linkedin';
+import { GithubSquare } from 'styled-icons/fa-brands/GithubSquare';
+import { Location } from 'styled-icons/icomoon/Location';
+
+import Divider from '../core/Divider';
+
 function Bio() {
   return (
     <StaticQuery
@@ -9,33 +16,34 @@ function Bio() {
       render={(data) => {
         const { author, social } = data.site.siteMetadata;
         return (
-          <div>
-            <Image
-              fixed={data.avatar.childImageSharp.fixed}
-              alt={author}
-              style={{
-                marginBottom: 0,
-                minWidth: 50,
-                borderRadius: '100%',
-              }}
-              imgStyle={{
-                borderRadius: '50%',
-              }}
-            />
-            <p>
-              Written by
-              {' '}
-              <strong>{author}</strong>
-              {' '}
-who lives and works in San Francisco building
-              useful things.
-              {' '}
-              <a href={`https://github.com/${social.github}`}>You should follow him on Twitter</a>
-              <a href={`https://www.linkedin.com/in/${social.linkin}`}>
-                You should follow him on Twitter
-              </a>
-            </p>
-          </div>
+          <section id="welcome" className="section welcome">
+            <div className="content">
+              <h5>Hello, I am </h5>
+              <h1>Michael Luo</h1>
+              <h5>Software Engineer</h5>
+              <Divider />
+              <ul className="nav contact-infos d-flex justify-content-between">
+                <li>
+                  <a className="" href="">
+                    <Location size="48" />
+                    <span>Syndey, NSW</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="http://magicmike90.github.io/">
+                    <GithubSquare size="48" />
+                    <span>MagicMike90.github.io</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="mailto:michael.luo0801@gmail">
+                    <Linkedin size="48" />
+                    <span>michael.luo0801@gmail.com</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </section>
         );
       }}
     />
---
+++ myblog/src/core/Divider.js
@@ -0,0 +1,6 @@
+import styled, { css } from 'styled-components';
+
+const Divider = styled.hr`
+  font-size: 24px;
+`;
+export default Divider;
