fd12ecd16a0b632d6857e8c626c127d893cff065
Michael Luo <michael.luo0801@gmail.com>
2019-01-30 10:57:49 +1100

    add responsive text

--- myblog/src/components/Bio.js
+++ myblog/src/components/Bio.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
-import styled from 'styled-components';
-import { shine } from '../core/Effect';
-import Divider from '../core/Divider';
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import styled from "styled-components";
+import { shine } from "../core/Effect";
+import Divider from "../core/Divider";
 
-const Content = styled.div`
-  font-size: 6rem;
-`;
+const Content = styled.div``;
 const ContentInner = styled.div`
   :hover {
     opacity: 1;
-    mask-image: linear-gradient(-75deg, rgba(0, 0, 0, 0.6) 30%, #000 50%, rgba(0, 0, 0, 0.6) 70%);
+    mask-image: linear-gradient(
+      -75deg,
+      rgba(0, 0, 0, 0.6) 30%,
+      #000 50%,
+      rgba(0, 0, 0, 0.6) 70%
+    );
     mask-size: 200%;
     animation: ${shine} 1.5s infinite;
   }
@@ -19,7 +22,7 @@ const StyledName = styled.p`
   margin: 0;
 `;
 const StyledSubtitle = styled.div`
-  font-size: 2rem;
+  font-size: 0.5em;
 `;
 function Bio() {
   return (
