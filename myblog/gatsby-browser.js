175064ecbcc5be96ea51b5dd32914e7e60ba57b3
Michael Luo <michael.luo0801@gmail.com>
2019-01-31 21:44:54 +1100

    update effect

--- myblog/gatsby-browser.js
+++ myblog/gatsby-browser.js
@@ -6,8 +6,11 @@ import React from "react";
 import posed, { PoseGroup } from "react-pose";
 import Layout from "./src/components/Layout";
 
+/**
+ * @dealy A duration, in milliseconds, to delay this transition. Does not affect children.
+ */
 const Transition = posed.div({
-  enter: { opacity: 1, delay: 150, beforeChildren: true, delayChildren: 0 },
+  enter: { opacity: 1, delay: 0, beforeChildren: true },
   exit: { opacity: 0 }
 });
 
